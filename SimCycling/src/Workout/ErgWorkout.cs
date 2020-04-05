f6fdeb932efd543f2a2d8ef3428bf8ef93bfd338
Damien <damien.wintz@gmail.com>
2020-04-05 23:54:44 +0200

    foundation for multiple workout formats

---
+++ SimCycling/src/Workout/ErgWorkout.cs
@@ -0,0 +1,15 @@
+namespace SimCycling.Workout
+{
+    public class ErgWorkout : GenericWorkout
+    {
+        public ErgWorkout()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
