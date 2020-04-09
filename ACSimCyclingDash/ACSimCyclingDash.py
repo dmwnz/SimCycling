c32b8a59a81003c4e86a98b412c24e8b1d71d84d
Damien <damien.wintz@gmail.com>
2020-04-09 23:01:14 +0200

    add user interaction from python app : select workout, stop workout, stop antmanager

--- ACSimCyclingDash/ACSimCyclingDash.py
+++ ACSimCyclingDash/ACSimCyclingDash.py
@@ -4,7 +4,6 @@ import mmap
 import subprocess
 import sys
 import acsys
-import signal
 import os
 
 antManagerExecutable = None
@@ -12,12 +11,12 @@ antManagerState      = None
 uiElements           = None
 workoutUi            = None
 
+SIGNAL_LOAD_WORKOUT  = b'\x01'
+SIGNAL_STOP_WORKOUT  = b'\x02'
+SIGNAL_EXIT          = b'\x03'
 
-
-    
 
 class RaceState:
-
     def _getMemoryMap(self):
         return mmap.mmap(0, 1024, "SimCyclingRaceState")
 
@@ -95,10 +94,10 @@ class AntManagerState:
         flag = memoryMap.read(1)
         if flag == b'\xAA':
             ac.console("Concurrent write in progress. Skip reading")
-            return
+            return False
         if flag != b'\x11':
             ac.console("Unknown signal flag. Skip reading")
-            return
+            return False
         memoryMap.seek(1)
         readBytes  = memoryMap.read()
         memoryMap.close()
@@ -106,6 +105,7 @@ class AntManagerState:
         #ac.console(readString)
         dictData   = json.loads(readString)
         self._instanciateFromDict(dictData)
+        return True
 
     def eraseMemory(self):
         memoryMap = self._getMemoryMap()
@@ -113,26 +113,53 @@ class AntManagerState:
         memoryMap.write(bytes(1024))
         memoryMap.close()
 
-def start(*args):
+def sendSignal(signal):
+    memoryMap = mmap.mmap(0, 1, "SimCyclingSignal")
+    memoryMap.seek(0)
+    memoryMap.write(signal)
+    memoryMap.close()
+
+def btn1_clicked(*args):
     global antManagerExecutable
-    ac.console("Hello start")
-    if antManagerExecutable is None:
-        ac.console("Starting exeinstance")
-        try:
-            antManagerExecutable = subprocess.Popen(r".\apps\python\ACSimCyclingDash\bin\SimCycling.exe", cwd=os.path.join(os.path.dirname(os.path.realpath(__file__)), "bin"))
-            ac.console("Executable launched : " + str(antManagerExecutable))
-        except Exception as e:
-            ac.log(repr(e))
-            ac.console(str(antManagerExecutable))
-
-def stop(*args):
+    if antManagerExecutable is None or antManagerExecutable.poll() is not None:
+        startExecutable()
+    elif not workoutInProgress():
+        loadWorkout()
+    else:
+        stopWorkout()
+ 
+def btn2_clicked(*args):
     global antManagerExecutable
-    ac.console("Hello stop")
-    if antManagerExecutable is not None:
-        antManagerExecutable.stdin.write("X")
-    antManagerExecutable = None
+    if antManagerExecutable is not None and antManagerExecutable.poll() is None:
+        stopExecutable()
 
+def workoutInProgress():
+    global antManagerState
+    return antManagerState is not None and antManagerState.RemainingTotalTime > 0
 
+def startExecutable():
+    global antManagerExecutable, uiElements
+    ac.console("Starting executable")
+    try:
+        antManagerExecutable = subprocess.Popen(r".\apps\python\ACSimCyclingDash\bin\SimCycling.exe", cwd=os.path.join(os.path.dirname(os.path.realpath(__file__)), "bin"))
+        ac.console("Executable launched : " + str(antManagerExecutable))
+        ac.setText(uiElements.btn1, "starting...")
+    except Exception as e:
+        ac.log(repr(e))
+        ac.console(str(antManagerExecutable))
+
+def loadWorkout():
+    ac.console("Sending signal to load workout")
+    sendSignal(SIGNAL_LOAD_WORKOUT)
+
+def stopWorkout():
+    ac.console("Sending signal to stop workout")
+    sendSignal(SIGNAL_STOP_WORKOUT)
+
+def stopExecutable():
+    global uiElements
+    ac.console("Sending signal to stop")
+    sendSignal(SIGNAL_EXIT)
 
 class UIElement():
     def __init__(self, valueName, appWindow, x, y, fontSize=48, unit="", format=None):
@@ -189,8 +216,8 @@ class UIElements:
 
         self.uiElements.append(TimerUIElement("TripTotalTime", appWindow, 166, 320, 32))
 
-        self.startButton   = ac.addButton(self.appWindow, "start")
-        self.stopButton    = ac.addButton(self.appWindow, "stop")
+        self.btn1   = ac.addButton(self.appWindow, "start")
+        self.btn2   = ac.addButton(self.appWindow, "stop")
 
     def setup(self):
         ac.addRenderCallback(self.appWindow, onRender)
@@ -203,16 +230,16 @@ class UIElements:
 
     
     def setupButtons(self):
-        ac.setSize(self.startButton, 165, 26)
-        ac.setSize(self.stopButton , 165, 26)
-        ac.setFontSize(self.startButton, 18)
-        ac.setFontSize(self.stopButton , 18)
-        ac.setFontAlignment(self.startButton, "center")
-        ac.setFontAlignment(self.stopButton, "center")
-        ac.setPosition(self.startButton, 1  , 378)
-        ac.setPosition(self.stopButton , 168, 378)
-        ac.addOnClickedListener(self.startButton, start)
-        ac.addOnClickedListener(self.stopButton , stop)
+        ac.setSize(self.btn1, 165, 26)
+        ac.setSize(self.btn2 , 165, 26)
+        ac.setFontSize(self.btn1, 18)
+        ac.setFontSize(self.btn2 , 18)
+        ac.setFontAlignment(self.btn1, "center")
+        ac.setFontAlignment(self.btn2, "center")
+        ac.setPosition(self.btn1, 1  , 378)
+        ac.setPosition(self.btn2 , 168, 378)
+        ac.addOnClickedListener(self.btn1, btn1_clicked)
+        ac.addOnClickedListener(self.btn2 , btn2_clicked)
 
     def update(self, antManagerState: AntManagerState):
         for el in self.uiElements:
@@ -264,22 +291,27 @@ def acMain(ac_version):
     workoutUi = WorkoutUI(workoutAppWindow)
     workoutUi.setup()
     
-
     return "ACSimCyclingDash"
 
 def onRender(*args):
-    global antManagerExecutable, antManagerState
+    global antManagerExecutable, antManagerState, uiElements
     try:
-        antManagerState.updateFromMemory()
+        stateFound = antManagerState.updateFromMemory()
+        if stateFound:
+            if not workoutInProgress():
+                ac.setText(uiElements.btn1, "load workout")
+            else:
+                ac.setText(uiElements.btn1, "stop workout")
+        elif not stateFound and ac.getText(uiElements.btn1) != "starting...":
+            ac.setText(uiElements.btn1, "start")
     except Exception as e:
         ac.console(repr(e))
+    
     antManagerState.TrackLength = ac.getTrackLength(0) / 1000
     uiElements.update(antManagerState)
     workoutUi.update(antManagerState)
     RaceState().updateToMemory()
-    if antManagerExecutable is not None and antManagerExecutable.poll() is not None:
-         antManagerExecutable = None
 
 def acShutdown():
     ac.log("BIKEDASH acShutdown")
-    #antManagerExecutable.kill()
+    sendSignal(SIGNAL_EXIT)
--- SimCycling/src/Launcher.cs
+++ SimCycling/src/Launcher.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Globalization;
+using System.IO.MemoryMappedFiles;
 using System.Threading;
-
+using System.Windows.Forms;
 
 namespace SimCycling
 {
 
+    enum Signal : byte
+    {
+        NONE = 0,
+        LOAD_WORKOUT = 1,
+        STOP_WORKOUT = 2,
+        EXIT = 3,
+    }
     
     class Launcher
     {
         static ANTDeviceManager manager;
         public static bool KeepRunning { get; set; } = true;
+        
+        [STAThread]
         static void Main(string[] args)
         {
             Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-US");
@@ -27,19 +37,59 @@ namespace SimCycling
             while (KeepRunning)
             {
                 Thread.Sleep(500);
-                cki = Console.ReadKey(true);
 
-                // Announce the name of the key that was pressed .
-                Console.WriteLine($"  Key pressed: {cki.Key}\n");
-
-                // Exit if the user pressed the 'X' key.
-                if (cki.Key == ConsoleKey.X) break;
+                ReadSignal();
             }
            
-
             manager.Stop();
             Console.WriteLine("exited gracefully");
         }
+
+        private static void ReadSignal()
+        {
+            var mm = MemoryMappedFile.CreateOrOpen("SimCyclingSignal", 1);
+            Signal readSignal = Signal.NONE;
+            using (var mmAccessor = mm.CreateViewAccessor())
+            {
+                readSignal = (Signal)mmAccessor.ReadByte(0);
+                mmAccessor.Write(0, (byte)Signal.NONE);
+            }
+            switch (readSignal)
+            {
+                case Signal.LOAD_WORKOUT:
+                    string filename = LoadWorkoutFile();
+                    if (filename != null)
+                    {
+                        manager.SetWorkout(filename);
+                    }
+                    break;
+                case Signal.STOP_WORKOUT:
+                    manager.StopWorkout();
+                    break;
+                case Signal.EXIT:
+                    KeepRunning = false;
+                    break;
+            }
+        }
+
+        private static string LoadWorkoutFile()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\..";
+                openFileDialog.Filter = "Zwift Workout files (*.zwo)|*.zwo|ERG workout files (*.erg)|*.erg|MRC workout files (*.mrc)|*.mrc";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    return openFileDialog.FileName;
+                }
+            }
+            return null;
+        }
+
         static void OnProcessExit(object sender, EventArgs e)
         {
             manager.Stop();
